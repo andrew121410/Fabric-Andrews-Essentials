570277e81aab3fa502faf12e29d5b9f1f6f2e9de
andrew121410 <andrewcherring@gmail.com>
2020-03-21 18:53:42 -0400

    Fix.

--- src/main/java/com/andrew121410/fabric/mixin/MixinServerLoginNetworkHandler.java
+++ src/main/java/com/andrew121410/fabric/mixin/MixinServerLoginNetworkHandler.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 public class MixinServerLoginNetworkHandler {
 
     @ModifyConstant(method = "tick", constant = @Constant(intValue = 600))
-    public int fix(){
+    public int fix(int old) {
         return 1200;
     }
 }
