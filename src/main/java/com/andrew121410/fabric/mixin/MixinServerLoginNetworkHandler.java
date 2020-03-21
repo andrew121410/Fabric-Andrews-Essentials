07c3e9151b5ca5ac939bee7eab89abfbc0dadd4b
andrew121410 <andrewcherring@gmail.com>
2020-03-21 18:18:12 -0400

    Fix so Germany players can join.

---
+++ src/main/java/com/andrew121410/fabric/mixin/MixinServerLoginNetworkHandler.java
@@ -0,0 +1,15 @@
+package com.andrew121410.fabric.mixin;
+
+import net.minecraft.server.network.ServerLoginNetworkHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ServerLoginNetworkHandler.class)
+public class MixinServerLoginNetworkHandler {
+
+    @ModifyConstant(method = "tick", constant = @Constant(intValue = 600))
+    public int fix(){
+        return 1200;
+    }
+}
